63bf077dcaca802b682cb474f8b31966e208fbee
“Abbie <abbie.coghlan@gmail.com>
2021-06-25 17:58:49 -0500

    API tests

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -13,8 +13,18 @@ RSpec.describe API do
             expect(API.input_is_valid?("123456")).to eq(false)
         end 
         it "should return true when the search term has less than 5 numbers" do
-            expect(API.input_is_valid?("123456")).to eq(false)
+            expect(API.input_is_valid?("1234")).to eq(true)
         end 
+    end
+
+    describe ".send_query" do
+        it "should return results" do
+            expect(API.send_query("Samantha Irby")).to_not be(nil)
+        end 
+        it "should return false if the user inputs invalid url symbols" do 
+            expect(API.send_query("*()&*(&^%$#@$%^&*()_+)*(&^%$#@%^")).to be(false)
+        end 
+
     end
 
 
