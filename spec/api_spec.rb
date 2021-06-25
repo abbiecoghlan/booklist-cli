01b17e1c2100f2f07212512bdee63772c390432e
“Abbie <abbie.coghlan@gmail.com>
2021-06-25 18:03:58 -0500

    specs completed

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -24,8 +24,14 @@ RSpec.describe API do
         it "should return false if the user inputs invalid url symbols" do 
             expect(API.send_query("*()&*(&^%$#@$%^&*()_+)*(&^%$#@%^")).to be(false)
         end 
-
     end
 
+    describe ".parse_results" do
+        it "should return an array of 5 book objects" do
+            expect(API.parse_results(API.send_query("Samantha Irby"))).to be_a_kind_of(Array)
+            expect(API.parse_results(API.send_query("Samantha Irby"))[0]).to be_an_instance_of(Book)
+            expect(API.parse_results(API.send_query("Samantha Irby")).length).to eq(5)
+        end 
+    end
 
 end
